7c57172b9433e55d3bd8ee79548dd2baa446e9f5
Hyeonjiwon <zlone1208@gmail.com>
2021-03-28 22:56:07 +0900

    modified file

---
+++ 문자열/1316/jiwon/Main.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        String str;
+        char [] word;
+        ArrayList list = new ArrayList();
+
+        // 문자열 읽기
+        // 한글자씩 다음 글자와 비교
+        // 만약 현재 글자와 다음 글자가 같으면 true  
+        // 다르면 false 
+    }
+}
